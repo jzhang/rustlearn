8633b77b45e59b9d49f88da8b7b68086b31fa61e
jzhang <zhangchunzhong@gmail.com>
2017-09-05 10:58:26 +0800

    add rust examples

---
+++ variables/src/main.rs
@@ -0,0 +1,26 @@
+
+
+fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+fn main() {
+    /*
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+    println!("The value of x is: {}", x);
+    
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+
+    let one = x.1;
+
+    println!("one {} , {}", one, add_one(5))
+    */
+    let a = [1, 2, 3, 4, 5];
+    //let index = 10;
+    let index = 3;
+    let element = a[index];
+
+    println!("The value of element is: {}", element);
+}
